8b7be555ead6781c94cc380bb4c498fea00bbbfd
RaamVijith <raamvijith1@gmail.com>
2023-07-16 23:42:30 +0530

    16/07/2023

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
                 <div className="caption">Welcome to Parallax web</div>
                 <div className="title">Dark Forest</div>
               </div>
+              <div className="img-layer layer-base" style={{backgroundImage: `url(${})`}}></div>
+              <div className="img-layer layer-mid" style={{backgroundImage: `url(${})`}}></div>
+              <div className="img-layer layer-front" style={{backgroundImage: `url(${})`}}></div>
+              <div className="img-layer layer-front" style={{backgroundImage: `url(${})`}}></div>
+
+
             </div>
           </header>
         </div>
